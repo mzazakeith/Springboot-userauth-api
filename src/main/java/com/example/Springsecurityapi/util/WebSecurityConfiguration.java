828f396dd78d46e66625a7042330fd3b2f2a2064
mzazakeith <mzazakeith@gmail.com>
2021-11-16 13:37:19 +0300

    custom user details added, service added

--- src/main/java/com/example/Springsecurityapi/util/WebSecurityConfiguration.java
+++ src/main/java/com/example/Springsecurityapi/util/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,5 +23,20 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .antMatchers("/user")
+                .hasAuthority("user")
+                .antMatchers("/admin")
+                .hasAuthority("admin")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 }
